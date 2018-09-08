12975468d2e105e59e05ea5ac362bf3645adad16
Alfredo.Salzillo <noctisbels@gmail.com>
2018-09-08 19:57:01 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    entry: {
+        main: './src/main.js',
+    },
+    output: {
+        filename: '[name].js',
+        path: __dirname,
+    },
+    module: {
+        rules: [{
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+        }],
+    },
+};
